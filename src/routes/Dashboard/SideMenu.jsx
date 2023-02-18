03df07fe1067db517462e5b3ac336e0cedaa3b5c
anthonyhu64 <anthonyhu64@gmail.com>
2023-02-18 13:32:59 -0800

    toggle removed due to bugs

--- src/routes/Dashboard/SideMenu.jsx
+++ src/routes/Dashboard/SideMenu.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { Link } from 'react-router-dom';
 
 function SideMenu() {
+  
   return (
     <Sidebar>
       <Menu>
-        <SubMenu label="Charts">
-          <MenuItem> Pie charts </MenuItem>
-          <MenuItem> Line charts </MenuItem>
+        <MenuItem component={<Link to="/Dashboard"/>}>Dashboard</MenuItem>
+        <SubMenu label="Product Database">
+          <MenuItem component={<Link to="/BrowseProducts"/>}> Browse Products</MenuItem>
+          <MenuItem component={<Link to="/ReportProducts"/>}> Report Products</MenuItem>
+          <MenuItem component={<Link to="/CreateEntry"/>}> Create Entry </MenuItem>
         </SubMenu>
-        <MenuItem> Documentation </MenuItem>
-        <MenuItem> Calendar </MenuItem>
+        <SubMenu label="Profile">
+          <MenuItem component={<Link to="/PersonalDetails"/>}> Personal Details</MenuItem>
+          <MenuItem component={<Link to="/ViewEntries"/>}> View Entries </MenuItem>
+        </SubMenu>
+        <MenuItem component={<Link to="/Settings"/>}> Settings </MenuItem>
+        <MenuItem component={<Link to="/"/>}> Signout </MenuItem>
         <KeyboardArrowRightIcon />
       </Menu>
     </Sidebar>
