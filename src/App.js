45237b5fa9a47c780a6e5bd2d70abaedadd18703
Braxton Goss <admin@admins-MacBook-Pro-2.local>
2023-02-18 10:05:33 -0800

    Dashboard added to routes + redux update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { React } from 'react';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from 'react-router-dom';
+import { React } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from './routes/Login/login.component';
-import ToDoListHome from './routes/ToDoListHome/to-do-list-home.component';
+import Login from "./routes/Login/Login";
+import ToDoListHome from "./routes/ToDoListHome/to-do-list-home.component";
 
-import './App.css';
+import "./App.css";
+import Dashboard from "./routes/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className='Root'>
-        <div className='App'>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element ={ <Login /> } />
-                    <Route exact path="/home" element ={ <ToDoListHome /> } />
-                </Routes>
-            </BrowserRouter>
-        </div>
+    <div className="Root">
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route exact path="/home" element={<ToDoListHome />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import Slice from "./slice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: Slice,
+  },
 });
